73e162838938c6f70bc71a9f3902fd5eecd0bdd1
augsaksham <sakshamdwivedi10@gmail.com>
2024-03-12 10:31:32 +0530

    added epochs converter and new prompt

--- mistral-finetuning/train.py
+++ mistral-finetuning/train.py
@@ -30,12 +30,32 @@ def download_dataset():
 
     if not os.path.exists('/training_data/data_train.jsonl'):
         def format_instruction(sample):
-            PROMPT_TEMPLATE = "[INST] <<SYS>>\nUse the provided tool and the to write an essay about any random challenge. While generating the essay include the provided personal information in the essay.\n<</SYS>>\n\nInput:\n Tool: {tool} \n Personal_Information: {pii} [/INST]\n\nEssay: {essay}"
+            PROMPT_TEMPLATE = """[INST] <<SYS>>
+            #Mission
+            You are a student which is tasked with writing an essay for a random Challenge using a given TOOL, also you need to use the provided PII in your essay.
+
+            #Context
+            - TOOL is a methodology used to write an essay.
+            - PII this contains a dictionary of personal information the dictionary can include the keys : ['MAIL', 'ID_NUM', 'NAME_STUDENT', 'PHONE_NUM', 'STREET_ADDRESS', 'URL_PERSONAL', 'USERNAME']
+            - Challenge is a random topic around which the essay is formulated.
+
+            #instructions
+            Step 0 : Analyze the provided Tool and the PII to get a gist of the type of essay that needs to be written. Note the keys of PII can have multiple values associated with them make sure to include all of them in the final essay.
+            Step 1 : Use the analysis in Step 0 to come up with a random challenge suiting the TOOL and the PII.
+            Step 2 : Write an essay in simple vocabulary on the chosen Challenge in Step1 using the TOOL and the PII make sure to include all the information present in the PII in the essay you can also repeat some of the information.
+            <</SYS>>
+
+            Input:
+            TOOL: {tool} 
+            PII: {pii} [/INST]
+
+            Essay: {essay}"""
+            # PROMPT_TEMPLATE = "[INST] <<SYS>>\nUse the provided tool and the to write an essay about any random challenge. While generating the essay include the provided personal information in the essay.\n<</SYS>>\n\nInput:\n Tool: {tool} \n Personal_Information: {pii} [/INST]\n\nEssay: {essay}"
             return {"text": PROMPT_TEMPLATE.format(tool=sample["TOOL"],pii=sample['PII'],essay=sample["full_text"])}
         
         # downloading data from hugging face
-        train_dataset = load_dataset('augsaksham/small_train', split='train')
-        val_dataset = load_dataset('augsaksham/small_train', split='validation')
+        train_dataset = load_dataset('augsaksham/full_train', split='train')
+        val_dataset = load_dataset('augsaksham/full_train', split='validation')
 
 
         train_dataset = train_dataset.map(format_instruction, remove_columns=['document', 'PII', 'TOOL','full_text','is_valid'])
@@ -87,6 +107,13 @@ def finetune(
     tokenizer = AutoTokenizer.from_pretrained(MODEL_PATH)
     tokenizer.pad_token = tokenizer.eos_token
 
+    def epochs_to_max_steps(num_epochs, total_train_samples, train_batch_size, gradient_accumulation_steps):
+        """Convert the number of epochs to the maximum number of training steps."""
+        steps_per_epoch = total_train_samples // (train_batch_size * gradient_accumulation_steps)
+        max_steps = num_epochs * steps_per_epoch
+        return max_steps
+    
+
     def tokenize(sample, cutoff_len=512, add_eos_token=True):
         prompt = sample["text"]
         result = tokenizer.__call__(
@@ -112,6 +139,15 @@ def finetune(
 
     tokenized_train_dataset = train_dataset.map(tokenize)
     tokenized_val_dataset = eval_dataset.map(tokenize)
+    
+    # Train Config
+    num_epochs = 3
+    total_train_samples = len(train_dataset) 
+    train_batch_size = 8
+    gradient_accumulation_steps = 4
+    max_steps = epochs_to_max_steps(num_epochs, total_train_samples, train_batch_size, gradient_accumulation_steps)
+    save_steps = max_steps//5
+    print("Maximum number of steps:", max_steps)
 
     model.gradient_checkpointing_enable()
     model = prepare_model_for_kbit_training(model)
@@ -172,16 +208,16 @@ def finetune(
         args=transformers.TrainingArguments(
             output_dir=f"/results/{run_id}",  # Must also set this to write into results Volume's mount location
             warmup_steps=5,
-            per_device_train_batch_size=8,
-            gradient_accumulation_steps=4,
-            max_steps=1000,  # Feel free to tweak to correct for under/overfitting
+            per_device_train_batch_size=train_batch_size,
+            gradient_accumulation_steps=gradient_accumulation_steps,
+            max_steps=max_steps,  # Feel free to tweak to correct for under/overfitting
             learning_rate=2e-5, # ~10x smaller than Mistral's learning rate
             bf16=True,
             optim="adamw_8bit",
             save_strategy="steps",       # Save the model checkpoint every logging step
-            save_steps=50,                # Save checkpoints every 50 steps
+            save_steps=save_steps,                # Save checkpoints every 50 steps
             evaluation_strategy="steps", # Evaluate the model every logging step
-            eval_steps=50,               # Evaluate and save checkpoints every 50 steps
+            eval_steps=save_steps,               # Evaluate and save checkpoints every 50 steps
             do_eval=True,                # Perform evaluation at the end of training
             report_to="wandb" if wandb_project else "",         
             run_name=run_id if wandb_project else ""     
@@ -209,3 +245,6 @@ def main(run_id: str = "", resume_from_checkpoint: str = None):
     finetune.remote(model_name=BASE_MODEL, run_id=run_id, wandb_project=WANDB_PROJECT, resume_from_checkpoint=resume_from_checkpoint)
     print(f"Completed training run {run_id=}")
     print("To test your trained model, run `modal run inference.py --run_id <run_id>`")
+    
+    
+# PROMPT_TEMPLATE = "[INST] <<SYS>>\nUse the provided tool and the to write an essay about any random challenge. While generating the essay include the provided personal information in the essay.\n<</SYS>>\n\nInput:\n Tool: {tool} \n Personal_Information: {pii} [/INST]\n\nEssay: {essay}"
